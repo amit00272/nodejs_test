df1addf65845a4c99b32e511cbbe14ca7cd0fa21
Amit Kumar <amit00272@hotmail.com>
2017-06-04 14:45:40 +0530

    After NPM install

--- FirstNodeJS/app.js
+++ FirstNodeJS/app.js
@@ -2,9 +2,19 @@ console.log("Start App");
 
 const fs= require("fs");
 const os =require("os");
+const notes=require("./note.js");
+const _=require("lodash");
 //1 way to concat the string
 //var str="Hello Mr. "+os.userInfo().username+"\n";
 //2 otherway to concat the string using templete string
-var str= `Hello ! ${os.userInfo().username}`;
-
+/*
+var str= `Hello ! ${os.userInfo().username} .You are ${notes.age} years old.\n`;
 fs.appendFile("greet.txt",str);
+*/
+//console.log(notes.add(10,20));
+//console.log(_.isString("true"));
+//console.log(_.isString(1));
+
+var filteredArray = _.uniq(["Amita","Ravi","Amit","amit",1,3,10,1,10,'a','a']);
+
+console.log(filteredArray);
---
+++ FirstNodeJS/note.js
@@ -0,0 +1,14 @@
+/**
+ * Created by noargs on 04/06/17.
+ */
+console.log("Starting notes.js");
+
+module.exports.age=25;
+
+module.exports.addNotes=() =>{
+
+    console.log("ad notes");
+    return "returning thing from adnotes";
+}
+
+module.exports.add = (a,b) => a+b;
