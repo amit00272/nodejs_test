7b7ffba2a79a70c9b7eb8c2f7926b4be1c3edc87
Amit Kumar <amit00272@hotmail.com>
2017-06-23 13:09:26 +0530

    url data

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,6 +1,6 @@
 var express=require('express');
 var bodyParser=require('body-parser');
-
+const {ObjectID} = require('mongodb');
 var {mongoose}=require('./db/db');
 var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
@@ -9,7 +9,6 @@ var app=express();
 
 app.use(bodyParser.json());
 
-//app.post('',(req,res)=>{})
 app.post('/todos',(req,res)=>{
 
     var todo =new Todo({
@@ -35,8 +34,26 @@ Todo.find().then((todos)=>{
 },(e)=>{
 res.sendStatus(400).send(e);
 });
+});
+
+app.get('/todos/:id',(req,res)=>{
+
+       var id=req.params.id;
+       if(!ObjectID.isValid(id))
+           return res.sendStatus(404).send();
+
+         Todo.findById(id).then((data)=>{
+
+             if(!data)
+                return res.sendStatus(404);
+
+             res.send({data});
+
+           }).catch((error)=>res.sendStatus(400).send({error}));
+
 
 });
+
 
 app.listen(3000,()=>{
 
